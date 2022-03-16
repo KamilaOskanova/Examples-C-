2eba46b457b6f1a08d93e4a7b4aba4358e680371
«Kamila» <koskanova@outlook.com>
2022-03-16 13:12:24 +0300

    Added Example 006 Condition If-Else

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter the username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else 
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
