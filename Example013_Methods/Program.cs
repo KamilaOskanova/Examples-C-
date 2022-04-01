5c3e2c64ee17bbd05e3fc0128fd84fcedd97fce8
«Kamila» <koskanova@outlook.com>
2022-04-02 02:13:01 +0300

    Added Example 013 Multiplication table

--- Example013_Methods/Program.cs
+++ Example013_Methods/Program.cs
@@ -36,7 +36,7 @@ int year = Method3();
 Console.WriteLine(year);
 */
 //Вид 4 - что-то принимают, что-то возвращают
-string Method4(int count, string text)
+/* string Method4(int count, string text)
 {
     int i = 0;
     string result = String.Empty;
@@ -47,12 +47,22 @@ string Method4(int count, string text)
         i++;
     }
     return result;
-   /* for (int i = 0; i < count; i++)
+   // for (int i = 0; i < count; i++)
     {
         result = result + text;
     }
-    */
+    //
 }
 
 string res = Method4 (10, "asdf");
 Console.WriteLine(res);
+*/
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
