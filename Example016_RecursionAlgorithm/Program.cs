72b948d64d90e90ba101d29f04f399273c4cbbbe
«Kamila» <koskanova@outlook.com>
2022-04-06 16:51:10 +0300

    Added Example 016 Recursion ALgorithm with two-dimensional array

---
+++ Example016_RecursionAlgorithm/Program.cs
@@ -0,0 +1,48 @@
+﻿string[,] table = new string[2, 5];
+// String.Empty
+// table[0, 0] table[0, 1] table[0, 2] table[0, 3] table[0, 4]
+// table[1, 0] table[1, 1] table[1, 2] table[1, 3] table[1, 4]   
+// две строки и пять столбцов, индексы нумеруются с нуля
+
+
+// table[1,2] = "cлово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+// matrix.GetLength(0) - указывает на количество строк, параметр 0 передаем для указания строк
+// matrix.GetLength(1) - указывает на количество столбцов, параметр 1 передаем для указания столбцов
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
